01995eb0c3fbd6f140e10602e3013e63e3f63b70
raimundo.marques <junnyhormarks@gmail.com>
2023-02-16 00:41:13 -0400

    frontend directory

---
+++ backend/bd.py
@@ -0,0 +1,48 @@
+Product = {
+    1: {
+        'name': 'Mouse',
+        'category': 'Periféricos',
+        'price': 2,
+        'serie': 123654
+    },
+    2: {
+        'name': 'Teclado',
+        'category': 'Periféricos',
+        'price': 5,
+        'serie': 321654
+    },
+    3: {
+        'name': 'Placa Mãe',
+        'category': 'Periféricos',
+        'price': 3,
+        'serie': 147258
+    },
+    4: {
+        'name': 'Flanela',
+        'category': 'Limpeza',
+        'price': 8,
+        'serie': 987321
+    },
+    5: {
+        'name': 'Adesivos de Times',
+        'category': 'Adesivos',
+        'price': 7,
+        'serie': 963258
+    },
+}
+
+
+Category = {
+    1: {
+        'name': 'Periféricos'
+    },
+    2: {
+        'name': 'Adesivos'
+    },
+    3: {
+        'name': 'Limpeza'
+    },
+    4: {
+        'name': 'Periféricos'
+    }
+}
---
+++ backend/main.py
@@ -0,0 +1,19 @@
+import uvicorn
+from fastapi import FastAPI
+from bd import Product
+from bd import Category
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return "Seja Bem-vindo a API de Produtos"
+
+
+@app.get("/product")
+def home_product():
+    return Product
+
+@app.get("/product/{id}")
+def product_id(id:int):
+    return Product[id]
